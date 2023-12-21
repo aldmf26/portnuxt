3895a9385865bfa026f83e137df2286783a25cd7
aldmf26 <aldimf26@gmail.com>
2023-12-21 16:43:37 +0800

    dsa

--- app.vue
+++ app.vue
@@ -1,10 +1,11 @@
 <script setup>
 // You might choose this based on an API call or logged-in status
 const layout = "default";
+
 </script>
 
 <template>
-  <NuxtLayout :name="layout">
+  <NuxtLayout :name="layout" >
     <NuxtPage />
   </NuxtLayout>
 </template>
