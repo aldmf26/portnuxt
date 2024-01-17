3e50f958c0b88ff7adf76188d68abb0ad6175937
Fahrizaldi <45739897+aldmf26@users.noreply.github.com>
2024-01-17 19:38:48 +0800

    tes

--- layouts/layout2.vue
+++ layouts/layout2.vue
@@ -27,6 +27,7 @@
       -->
     </div>
     <div class="">
+    <--! 
       <div class="dropdown">
         <div tabindex="0" role="button" class="btn btn-sm m-1">
           Theme
@@ -41,6 +42,30 @@
           </li>
         </ul>
       </div>
+      -->
+      <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Dropdown button <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+      </svg>
+      </button>
+
+      <!-- Dropdown menu -->
+      <div id="dropdown" class="z-10 hidden bg-primary divide-y rounded-lg shadow w-44">
+          <ul class="py-2 text-sm " aria-labelledby="dropdownDefaultButton">
+            <li>
+              <a href="#" class="block px-4 py-2">Dashboard</a>
+            </li>
+            <li>
+              <a href="#" class="block px-4 py-2">Settings</a>
+            </li>
+            <li>
+              <a href="#" class="block px-4 py-2">Earnings</a>
+            </li>
+            <li>
+              <a href="#" class="block px-4 py-2">Sign out</a>
+            </li>
+          </ul>
+      </div>
+
     </div>
   </div>
 
