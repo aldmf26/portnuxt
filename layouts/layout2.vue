f513a5c410335617d9d348a58188f0453f2d417d
aldimf26 <aldimf26@gmail.com>
2024-01-17 20:53:07 +0800

    update nav

--- layouts/layout2.vue
+++ layouts/layout2.vue
@@ -28,11 +28,11 @@
     </div>
     <div class="">
 
-      <div class=" relative inline-block text-left dropdown">
+      <div class="relative inline-block text-left dropdown">
         <span class="rounded-md shadow-sm">
           <button
           @click="isDropdownOpen = ! isDropdownOpen"
-            class="inline-flex justify-center w-full px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800"
+            class="inline-flex justify-center w-full px-4 py-2 text-sm font-medium leading-5 text-base-500 transition duration-150 ease-in-out bg-primary border border-gray-300 rounded-md "
             type="button" aria-haspopup="true" aria-expanded="true" aria-controls="headlessui-menu-items-117">
             <span>Options</span>
             <svg class="w-5 h-5 ml-2 -mr-1" viewBox="0 0 20 20" fill="currentColor">
@@ -46,36 +46,24 @@
         v-show="isDropdownOpen"
           class="dropdown-menu transition-all duration-300 transform origin-top-right -translate-y-2 scale-95">
           <div
-            class="absolute right-0 w-56 mt-2 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none"
+            class="absolute right-0 w-56 mt-2 origin-top-right bg-primary border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none"
             aria-labelledby="headlessui-menu-button-1" id="headlessui-menu-items-117" role="menu">
-            <div class="px-4 py-3">
-              <p class="text-sm leading-5">Signed in as</p>
-              <p class="text-sm font-medium leading-5 text-gray-900 truncate">tom@example.com</p>
-            </div>
+        
             <div class="py-1">
-              <a href="javascript:void(0)" tabindex="0"
-                class="text-gray-700 flex justify-between w-full px-4 py-2 text-sm leading-5 text-left"
-                role="menuitem">Account settings</a>
-              <a href="javascript:void(0)" tabindex="1"
-                class="text-gray-700 flex justify-between w-full px-4 py-2 text-sm leading-5 text-left"
-                role="menuitem">Support</a>
-              <span role="menuitem" tabindex="-1"
-                class="flex justify-between w-full px-4 py-2 text-sm leading-5 text-left text-gray-700 cursor-not-allowed opacity-50"
-                aria-disabled="true">New feature (soon)</span>
-              <a href="javascript:void(0)" tabindex="2"
-                class="text-gray-700 flex justify-between w-full px-4 py-2 text-sm leading-5 text-left"
-                role="menuitem">License</a>
+              <div v-for="theme in themes" :key="theme">
+                <a @click="changeTheme(theme)" tabindex="1"
+                  class="text-white cursor-pointer hover:bg-info flex justify-between w-full px-4 py-2 text-sm leading-5 text-left"
+                  role="menuitem">
+                   {{theme}}
+                </a>
+              </div>
             </div>
-            <div class="py-1">
-              <a href="javascript:void(0)" tabindex="3"
-                class="text-gray-700 flex justify-between w-full px-4 py-2 text-sm leading-5 text-left"
-                role="menuitem">Sign out</a>
-            </div>
+          
           </div>
         </div>
       </div>
 
-      <!-- <div class="dropdown">
+      <div class="dropdown">
         <div tabindex="0" role="button" class="btn btn-sm m-1">
           Theme
           <svg width="12px" height="12px" class="h-2 w-2 fill-current opacity-60 inline-block"
@@ -88,7 +76,7 @@
             <a @click="changeTheme(theme)">{{ theme }}</a>
           </li>
         </ul>
-      </div> -->
+      </div>
     </div>
   </div>
 
