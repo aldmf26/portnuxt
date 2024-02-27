8b32135c006b906184a8570d7ce2206f2d7382b9
aldmf26 <aldimf26@gmail.com>
2024-02-27 11:43:58 +0800

    dibulikan css

--- app.vue
+++ app.vue
@@ -3,12 +3,3 @@
         <NuxtPage />
     </NuxtLayout>
 </template>
-
-<script setup>
-useHead({
-    link:{
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap'
-    }
-})
-</script>
