df3f8c672df72ea5dab194e9e4ad6e5342c83a08
aldmf26 <aldimf26@gmail.com>
2024-02-27 11:39:42 +0800

    update css

--- app.vue
+++ app.vue
@@ -4,3 +4,11 @@
     </NuxtLayout>
 </template>
 
+<script setup>
+useHead({
+    link:{
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap'
+    }
+})
+</script>
