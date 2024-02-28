d2a953656a058f7d1e9d97f1e9111f286f972bba
aldmf26 <aldimf26@gmail.com>
2024-02-28 16:40:40 +0800

    upd

--- layouts/layout2.vue
+++ layouts/layout2.vue
@@ -88,7 +88,7 @@
   </div>
 
   <div class="fixed bottom-4 right-4" id="scrollToTopBtn" @click="scrollToTop">
-    <svg class="btn btn-info btn-sm" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
+    <svg class="btn btn-primary btn-sm" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
       xmlns:xlink="http://www.w3.org/1999/xlink">
       <defs>
         <path id="top-a"
